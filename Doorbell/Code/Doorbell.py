1a2fc5740b870b77e3cb76f36def8444e3492287
Houwaida Hidri <houwaidahidri@gmail.com>
2025-08-19 14:42:32 +0100

    Update Doorbell.py

--- Doorbell/Code/Doorbell.py
+++ Doorbell/Code/Doorbell.py
@@ -1,1 +1,28 @@
-
+from gpiozero import Buzzer, Button   
+import time 
+ 
+buzzer = Buzzer(17) 
+button = Button(18) 
+ 
+def onButtonPressed(): 
+    buzzer.on() 
+    print("Button is pressed, buzzer turned on >>>") 
+     
+def onButtonReleased(): 
+    buzzer.off() 
+    print("Button is released, buzzer turned on <<<") 
+ 
+def loop(): 
+    button.when_pressed = onButtonPressed 
+    button.when_released = onButtonReleased 
+    while True : 
+        time.sleep(2) 
+         
+def destroy(): 
+    buzzer.close() 
+    button.close() 
+ 
+try:
+  loop() 
+except KeyboardInterrupt:
+  destroy() 
